4da1d0349111ac0c1b66107dee634995706a312d
LeeHyeSu <hsy4896@daum.net>
2021-06-14 18:22:24 +0900

    Initial commit

---
+++ src/main/java/com/podomarket/podomarket/PodomarketApplication.java
@@ -0,0 +1,13 @@
+package com.podomarket.podomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodomarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodomarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/podomarket/podomarket/PodomarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podomarket.podomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodomarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
